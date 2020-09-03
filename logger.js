f9cd74f03a9eac681ff0b47dc07aa2a9aaf8fdc8
yousuf <yousuf3592@gmail.com>
2020-09-03 19:32:29 +0530

    SEP-03 Practice

--- logger.js
+++
@@ -1,6 +0,0 @@
-var url = "http://mylogger.io/log";
-
-function log(msg) {
-  console.log(msg);
-}
-module.exports.log = log;
--- osSysmod.js
+++
@@ -1,6 +0,0 @@
-const os = require("os");
-
-var totalmem = os.totalmem();
-var freemem = os.freemem();
-
-console.log(totalmem, freemem);
